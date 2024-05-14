4e33b3bfed6894341cef84448250b29a96959e95
crmarm <crmarm313@gmail.com>
2024-05-14 10:23:12 +0000

    tracking

--- views/site/railfreght.php
+++ views/site/railfreght.php
@@ -1,6 +1,10 @@
 <?php $this->params['title'] = @$GLOBALS['text']['Rail_Freight_']; ?>
 <?php $this->params['second'] =  @$GLOBALS['text']['__SERVICES__']; ?>
+<<<<<<< Updated upstream
 <?php $this->params['href'] =  Yii::$app->urlManager->createUrl('services')  ?>/<?= $_COOKIE['language'] ?>
+=======
+<?php $this->params['href'] =  '/services' ?>
+>>>>>>> Stashed changes
 
 <a  data-aos="fade-right" data-aos-duration="2000" class="go-to-up" href="#top">
     <button >
@@ -82,7 +86,11 @@
 
                         </ul>
                         <button class="border-0"  type="submit">
+<<<<<<< Updated upstream
                             <a href="<?= Yii::$app->urlManager->createUrl('contact')  ?>/<?= $_COOKIE['language'] ?>">
+=======
+                            <a href="/contact">
+>>>>>>> Stashed changes
                                 <?= @$GLOBALS['text']['Contact_Us_Title'] ?>
                             </a>
                         </button>
